18605cbfc85e052aad54dc7185b829cff7619865
Wiktor Nobis <nobis171@wp.pl>
2022-04-13 17:15:11 +0200

    app split into components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,76 @@
-
-import './App.css';
+import './App.scss';
 import {useState} from "react";
+
+import Header from './components/Header';
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+
+
+function* gen() {
+    let id = 0;
+    while (true) {
+        yield id;
+        id++;
+    }
+}
+
+const g = gen();
+
 
 function App() {
     const [value, setValue] = useState('');
     const [tasks, setTasks] = useState([]);
+    const [status, setStatus] = useState('all');
+
     const handleInput = (event) => {
         setValue(event.target.value);
     }
 
     const handleAddTask = (event) => {
-        if (event.key === 'Enter'){
-            setTasks([...tasks, value]);
+        if (event.key === 'Enter' && value !== '') {
+            setTasks([...tasks, {
+                id: g.next().value,
+                name: value,
+                status: false
+            }]);
             setValue('');
         }
     }
 
-  return (
-    <div className="App">
-      <h1>Todo</h1>
-        <input
-            type='text'
-            value={value}
-            onChange={handleInput}
-            onKeyUp={handleAddTask}
-        />
-        <h2>{value}</h2>
-    </div>
-  );
+    const removeTask = (task) => {
+        setTasks(tasks.filter((item) => item !== task));
+    }
+
+    const changeStatus = (task) => {
+        setTasks(tasks.map((item) => {
+            if (item === task) {
+                item.status = !item.status
+            }
+
+            return item;
+        }))
+    }
+
+    const clearCompleted = () => {
+        setTasks(tasks.filter((item) => !item.status));
+    }
+
+    return (
+        <div className="App">
+            <Header/>
+            <TaskInput value={value} handleInput={handleInput} handleAddTask={handleAddTask}/>
+            <TaskList status={status} tasks={tasks} changeStatus={changeStatus} removeTask={removeTask}/>
+
+            {tasks.filter((task) => task.status).length ?
+                <button onClick={clearCompleted}>Remove Completed</button> : ''}
+            <div>
+                <button onClick={() => setStatus('all')}>All</button>
+                <button onClick={() => setStatus(false)}>Active</button>
+                <button onClick={() => setStatus(true)}>Done</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
