a878b924ae200d625150cf155ad372da013a40e5
Wiktor Nobis <nobis171@wp.pl>
2022-04-13 13:06:05 +0200

    most important react functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const handleInput = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleAddTask = (event) => {
+        if (event.key === 'Enter'){
+            setTasks([...tasks, value]);
+            setValue('');
+        }
+    }
+
   return (
     <div className="App">
       <h1>Todo</h1>
+        <input
+            type='text'
+            value={value}
+            onChange={handleInput}
+            onKeyUp={handleAddTask}
+        />
+        <h2>{value}</h2>
     </div>
   );
 }
